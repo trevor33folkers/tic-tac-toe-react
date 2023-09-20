21fe1a44360dc7e95cbff7a7bd7436967ae7baf8
Trevor Folkers <travesty3313@gmail.com>
2023-09-19 22:09:42 -0400

    initial commit

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function Square() {
+  const[value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  )
+}
+
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null);
+  return (
+    <>
+      <div class="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div class="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div class="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
